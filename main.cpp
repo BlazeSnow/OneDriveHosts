043c2bf56c82b16b52830595370edc6423b8b838
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-09-15 22:48:01 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <system_error>
+#include <cstdlib>
 
 std::vector<asio::ip::tcp::endpoint> get_all_ips(const std::string &domain)
 {
@@ -11,14 +12,30 @@ std::vector<asio::ip::tcp::endpoint> get_all_ips(const std::string &domain)
     std::vector<asio::ip::tcp::endpoint> endpoints;
     try
     {
+        // 先创建名称服务器的endpoint
+        asio::ip::address name_server_addr = asio::ip::address::from_string("1.1.1.1");
+        asio::ip::tcp::endpoint name_server(name_server_addr, 53);
+
         asio::ip::tcp::resolver::query query(domain, "");
-        asio::ip::tcp::resolver::iterator it = resolver.resolve(query);
-        asio::ip::tcp::resolver::iterator end;
-        while (it != end)
-        {
-            endpoints.push_back(*it);
-            ++it;
-        }
+        // 使用async_resolve并提供自定义的resolve_handler
+        resolver.async_resolve(query, name_server,
+                               [&endpoints](const std::error_code &ec, asio::ip::tcp::resolver::iterator it)
+                               {
+                                   if (!ec)
+                                   {
+                                       asio::ip::tcp::resolver::iterator end;
+                                       while (it != end)
+                                       {
+                                           endpoints.push_back(*it);
+                                           ++it;
+                                       }
+                                   }
+                                   else
+                                   {
+                                       std::cerr << "Error resolving domain: " << ec.message() << std::endl;
+                                   }
+                               });
+        io.run();
     }
     catch (const std::system_error &e)
     {
