d926fc6b372ef80fb7bd484941dac0008b38965c
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-09-20 10:17:33 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,70 @@
 #include <QCoreApplication>
-#include <QDebug>
 #include <QHostInfo>
+#include <QHostAddress>
+#include <QList>
+#include <QTextStream>
+#include <iostream>
+
+// 获取用户输入需要排除的IP地址
+QList<QHostAddress> getExcludedIPs()
+{
+    QList<QHostAddress> excludedIPs;
+    QTextStream qin(stdin);
+    QString input;
+
+    std::cout << "输入需要排除的IP地址，输入'q'完成输入：" << std::endl;
+    while (true)
+    {
+        std::cout << "排除的IP: ";
+        input = qin.readLine().trimmed();
+        if (input == "q")
+        {
+            break;
+        }
+        QHostAddress address(input);
+        if (!address.isNull())
+        {
+            excludedIPs.append(address);
+        }
+        else
+        {
+            std::cout << "无效的IP地址，请重试。" << std::endl;
+        }
+    }
+    return excludedIPs;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QString url = "example.com"; // 这里替换为实际的网址
-    QHostInfo info = QHostInfo::fromName(url);
-    QList<QHostAddress> addrs = info.addresses();
-    qDebug() << "All IP addresses for " << url << ":";
-    for (int i = 0; i < addrs.size(); ++i)
+
+    QTextStream qin(stdin);
+    QString hostname;
+
+    // 询问用户输入网址
+    std::cout << "请输入网址: ";
+    hostname = qin.readLine().trimmed();
+
+    // 获取所有IP地址
+    QHostInfo hostInfo = QHostInfo::fromName(hostname);
+
+    if (hostInfo.error() != QHostInfo::NoError)
     {
-        qDebug() << addrs.at(i).toString();
+        std::cerr << "无法解析主机名: " << hostInfo.errorString().toStdString() << std::endl;
+        return -1;
     }
-    // 假设这里有用户输入要排除的IP地址列表，为了简单演示，这里直接写死要排除的IP
-    QStringList excludeList;
-    excludeList << "192.168.1.1";
-    qDebug() << "IP addresses after exclusion:";
-    for (int i = 0; i < addrs.size(); ++i)
+
+    QList<QHostAddress> excludedIPs = getExcludedIPs();
+
+    // 输出所有不在排除列表中的IP地址
+    std::cout << "IP地址列表:" << std::endl;
+    for (const QHostAddress &address : hostInfo.addresses())
     {
-        QString ip = addrs.at(i).toString();
-        if (!excludeList.contains(ip))
+        if (!excludedIPs.contains(address))
         {
-            qDebug() << ip;
+            std::cout << address.toString().toStdString() << std::endl;
         }
     }
-    return a.exec();
+
+    return 0;
 }
