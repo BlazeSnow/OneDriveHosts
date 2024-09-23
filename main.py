fe5109a1168587d11ac0281df6ee15aa56c7a304
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-09-23 16:28:54 +0800

    Update main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import socket
+
+
+def get_ipv4_addresses(domain):
+    try:
+        addr_info = socket.getaddrinfo(domain, None)
+        ipv4_addresses = [addr[4][0]
+                          for addr in addr_info if addr[0] == socket.AF_INET]
+        return ipv4_addresses
+    except socket.gaierror:
+        return []
+
+
+domain = "github.com"
+ips = get_ipv4_addresses(domain)
+for ip in ips:
+    print(ip)
