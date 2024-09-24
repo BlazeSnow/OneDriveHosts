c72524e525c18e97630526a0581363cf18296c2f
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-09-25 07:34:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,27 @@
-import dns.resolver
+import requests
+import json
 
 
-def get_ip_addresses_using_dnspython(domain_list):
-    resolver = dns.resolver.Resolver()
-    resolver.nameservers = ['1.1.1.1']
-    ips = []
-    for domain in domain_list:
-        try:
-            answers = resolver.resolve(domain)
-            for rdata in answers:
-                ips.append(rdata.address)
-        except dns.resolver.NXDOMAIN:
-            print(f"域名 {domain} 不存在。")
-        except dns.resolver.NoAnswer:
-            print(f"无法获取域名 {domain} 的 IP 地址。")
-    return ips
+def resolve_domain_via_doh(domain):
+    url = "https://dns.google/resolve?name={}&type=A".format(domain)
+    response = requests.get(url, headers={"accept": "application/dns-json"})
+    if response.status_code == 200:
+        data = json.loads(response.content)
+        answers = data.get('Answer', [])
+        ips = []
+        for answer in answers:
+            if answer['type'] == 1:
+                ips.append(answer['data'])
+        return ips
+    else:
+        return None
 
 
-domain_list = ['example.com', 'anotherdomain.com']
-ips = get_ip_addresses_using_dnspython(domain_list)
-print(ips)
+domains = ['api.onedrive.com',
+           'chi01pap001.storage.live.com', 'd.docs.live.net']
+for domain in domains:
+    ips = resolve_domain_via_doh(domain)
+    if ips:
+        print(f"域名 {domain} 的 IP 地址为：{ips}")
+    else:
+        print(f"无法获取域名 {domain} 的 IP 地址。")
